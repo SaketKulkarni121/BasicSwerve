1fa627441b557f1ed237b34b4837e0264399e35c
CHACK0LATE <joshuavchacko@gmail.com>
2021-07-29 09:47:51 -0400

    Revert "Merge branch 'master' of https://github.com/SaketKulkarni121/BasicSwerve"
    This reverts commit c9937d8399ffcbe3b7880c435e39aef9385d56a0, reversing
    changes made to cfc42205353107e4e9342398c47a85a300775d1a.

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -4,7 +4,8 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import edu.wpi.first.wpilibj.XboxController;
 
 public class DriveTrain {
 
