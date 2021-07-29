6e7848b7f73bba495f1243cc700b1617d23cc591
Saket Kulkarni <31022476+SaketKulkarni121@users.noreply.github.com>
2021-07-29 09:04:34 -0400

    Fix this

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,37 @@
+package frc.robot;
+
+public class Constants {
+    
+	public final static int MOTOR_MAX_RPM = 1;
+	public final static int MOTOR_MIN_RPM = 0;
+
+	//Big Bird
+	public final static int DT_BB_DRIVE_TALON_ID = 20;
+	public final static int DT_BB_TURN_TALON_ID = 1;
+	public final static double DT_BB_ABS_ZERO = 0.874969474969475;
+
+	//Big Horse
+	public final static int DT_BH_DRIVE_TALON_ID = 13;
+	public final static int DT_BH_TURN_TALON_ID = 12;
+	public final static double DT_BH_ABS_ZERO = 0.5032967032967033;
+	
+
+	//Big Giraffe
+	public final static int DT_BG_DRIVE_TALON_ID = 3;
+	public final static int DT_BG_TURN_TALON_ID = 2;
+	public final static double DT_BG_ABS_ZERO = 0.31306471306471306;
+
+
+	//Big Sushi
+	public final static int DT_BS_DRIVE_TALON_ID = 14;
+	public final static int DT_BS_TURN_TALON_ID = 15;
+    public final static double DT_BS_ABS_ZERO = 0.5343101343101343;
+    
+    public static final double DRIVER_DEAD_ZONE = 0.15;
+
+	public static final double DRIVER_ROT_DEAD_ZONE = 0.1;
+	
+	public static final int XBOX_DRIVER = 0;
+
+	public static final int XBOX_MANIP = 1;
+}
---
+++ src/main/java/frc/robot/DriveTrain.java
@@ -0,0 +1,96 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class DriveTrain {
+
+    public static DriveTrain instance;
+    public TalonSRX t_frontLeft, t_frontRight, t_backLeft, t_backRight;
+    public CANSparkMax d_frontLeft, d_frontRight, d_backLeft, d_backRight;
+    // Big Horse, Big Giraffe, Big Sushi, Big Bird
+
+    public static DriveTrain getInstance() {
+        if (instance == null)
+            instance = new DriveTrain();
+        return instance;
+    }
+
+    public DriveTrain() {
+
+        d_backRight = new CANSparkMax(Constants.DT_BB_DRIVE_TALON_ID, MotorType.kBrushless);
+        d_frontLeft = new CANSparkMax(Constants.DT_BH_DRIVE_TALON_ID, MotorType.kBrushless);
+        d_frontRight = new CANSparkMax(Constants.DT_BG_DRIVE_TALON_ID, MotorType.kBrushless);
+        d_backLeft = new CANSparkMax(Constants.DT_BS_DRIVE_TALON_ID, MotorType.kBrushless);
+
+        t_backRight = new TalonSRX(Constants.DT_BB_TURN_TALON_ID);
+        t_frontLeft = new TalonSRX(Constants.DT_BH_TURN_TALON_ID);
+        t_frontRight = new TalonSRX(Constants.DT_BG_TURN_TALON_ID);
+        t_backLeft = new TalonSRX(Constants.DT_BS_TURN_TALON_ID);
+
+    }
+
+    public void setDrivePower(double backRightPower, double backLeftPower, double frontLeftPower,
+            double frontRightPower) {
+        d_backRight.set(backRightPower);
+        d_backLeft.set(backLeftPower);
+        d_frontLeft.set(frontLeftPower);
+        d_frontRight.set(frontRightPower);
+    }
+
+    public void setTurnPower(double backRightPower, double backLeftPower, double frontLeftPower,
+            double frontRightPower) {
+        t_backRight.set(ControlMode.PercentOutput, backRightPower);
+        t_backLeft.set(ControlMode.PercentOutput, backLeftPower);
+        t_frontLeft.set(ControlMode.PercentOutput, frontLeftPower);
+        t_frontRight.set(ControlMode.PercentOutput, frontRightPower);
+    }
+
+    public void tankDrive(double left, double right) {
+        setDrivePower(right, left, left, right);
+    }
+
+    /*     public void stopDrive() {
+        setDrivePower(0, 0, 0, 0);
+    }
+
+    public void setAllIdleMode(boolean brake) {
+        if (brake) {
+            d_backLeft.setIdleMode(IdleMode.kBrake);
+            d_backRight.setIdleMode(IdleMode.kBrake);
+            d_frontRight.setIdleMode(IdleMode.kBrake);
+            d_frontLeft.setIdleMode(IdleMode.kBrake);
+        } else {
+            d_backLeft.setIdleMode(IdleMode.kCoast);
+            d_backRight.setIdleMode(IdleMode.kCoast);
+            d_frontRight.setIdleMode(IdleMode.kCoast);
+            d_frontLeft.setIdleMode(IdleMode.kCoast);
+        }
+    }
+
+    public void humanDrive(double fwd, double str, double rot) {
+        if (Math.abs(fwd) < Constants.DRIVER_DEAD_ZONE && Math.abs(str) < Constants.DRIVER_DEAD_ZONE
+                && Math.abs(rot) < Constants.DRIVER_ROT_DEAD_ZONE) {
+
+            setAllIdleMode(true);
+            stopDrive();
+
+        } else {
+            setAllIdleMode(false);
+
+            swerveDrive(Utils.expoDeadzone(Sensitivity.LOW, Constants.DRIVER_DEAD_ZONE, fwd),
+                    Utils.expoDeadzone(Sensitivity.LOW, Constants.DRIVER_DEAD_ZONE, str),
+                    Utils.expoDeadzone(Sensitivity.LOW, Constants.DRIVER_ROT_DEAD_ZONE, rot));
+        }
+    }
+
+    public void fieldCentricDrive(double fwd, double str, double rot) {
+        double temp = (fwd * Math.cos(getgyroAngleInRad())) + (str * Math.sin(getgyroAngleInRad()));
+        str = (-fwd * Math.sin(getgyroAngleInRad())) + (str * Math.cos(getgyroAngleInRad()));
+        fwd = temp;
+        humanDrive(fwd, str, rot);
+    }*/
+
+}
